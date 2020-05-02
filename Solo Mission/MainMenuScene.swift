98722f923517bc6b6c1f186ca3b3db007875cc43
Stephen Jackson <jjackson19862017@gmail.com>
2020-05-02 22:44:50 +0100

    Created Main Menu

---
+++ Solo Mission/MainMenuScene.swift
@@ -0,0 +1,74 @@
+//
+//  MainMenuScene.swift
+//  Solo Mission
+//
+//  Created by Stephen Jackson on 02/05/2020.
+//  Copyright © 2020 Stephen Jackson. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class mainMenuScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+        
+        let gameBy = SKLabelNode(fontNamed: "The Bold Font")
+        gameBy.text = "Stephen Jackson"
+        gameBy.fontSize = 50
+        gameBy.fontColor = SKColor.white
+        gameBy.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.78)
+        gameBy.zPosition = 1
+        self.addChild(gameBy)
+        
+        let gameName1 = SKLabelNode(fontNamed: "The Bold Font")
+        gameName1.text = "SOLO"
+        gameName1.fontSize = 200
+        gameName1.fontColor = SKColor.white
+        gameName1.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.7)
+        gameName1.zPosition = 1
+        self.addChild(gameName1)
+        
+        let gameName2 = SKLabelNode(fontNamed: "The Bold Font")
+        gameName2.text = "MISSION"
+        gameName2.fontSize = 200
+        gameName2.fontColor = SKColor.white
+        gameName2.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.625)
+        gameName2.zPosition = 1
+        self.addChild(gameName2)
+        
+        let startGame = SKLabelNode(fontNamed: "The Bold Font")
+        startGame.text = "Start Game"
+        startGame.fontSize = 150
+        startGame.fontColor = SKColor.white
+        startGame.position = CGPoint(x: self.size.width*0.5, y: self.size.height*0.4)
+        startGame.zPosition = 1
+        startGame.name = "startbutton"
+        self.addChild(startGame)
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch: AnyObject in touches{
+            let pointOfTouch = touch.location(in: self)
+            let nodeITapped = atPoint(pointOfTouch)
+            
+            if nodeITapped.name == "startbutton" {
+                let sceneToMoveTo = GameScene(size: self.size)
+                sceneToMoveTo.scaleMode = self.scaleMode
+                let myTransition = SKTransition.fade(withDuration: 0.5)
+                self.view!.presentScene(sceneToMoveTo, transition: myTransition)
+            }
+            
+            
+        }
+    }
+    
+}
